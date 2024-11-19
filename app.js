44de22e24449bb0bd055f4c1c303d91398b3a7db
Devtaitv <taikaka2015@gmail.com>
2024-11-19 14:47:58 +0700

    restapi

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const feedRoutes = require('./routes/feed');
 const app = express();
+
+//app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); //application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use((req,res,next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE');
@@ -10,9 +19,23 @@ app.use((req,res,next) =>{
     next();
 });
 
-
-//app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); //application/json
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
+
+
+mongoose
+    .connect(
+    'mongodb+srv://tester:faaJ8OzQ29fMHAIz@cluster0.maq21.mongodb.net/feeds?retryWrites=true&w=majority&appName=Cluster0'
+    )
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,76 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) =>{
-    res.status(200).json({
-        posts: [{ title : 'First Post', content: 'This is the first posts!' }]
-    });
+    Post.find()
+        .then(posts =>{
+            if(!posts){
+                const error = new Error('Post not found');
+                error.status = 401;
+                throw error;
+            }
+            res.status(200).json({
+                message: 'Post found success',
+                posts: posts });
+        })
+        .catch(err => {
+            if (!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
 
 exports.createPost = (req, res, next) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const error = new Error('Validation failed, entered data is incorrect');
+        error.status = 422;
+        throw error;
+    }
     const title = req.body.title;
     const content = req.body.content;
-    console.log(title, content);
-    res.status(201).json({
-        message: 'Post created success',
-        post: { id: new Date().toISOString(), title: title, content: content }
-    })
+    const post = new Post({
+        title,
+        content,
+        imageUrl: 'images/img.png',
+        creator: {
+            name: 'Tai Van',
+        },
+    });
+    post
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Post created success',
+                post: result
+            })
+        })
+        .catch(err => {
+            if (!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
+exports.getPost = (req, res, next) =>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post =>{
+            if(!post){
+                const error = new Error('Post not found');
+                error.status = 401;
+                throw error;
+            }
+            res.status(200).json({ message: 'Post found success', post: post });
+        })
+        .catch(err => {
+            if (!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
